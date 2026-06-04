c5937f8e1f18bb958801026a93440a401ed915ff
edmilson-prog <edmilson@ailainteligente.com>
2026-06-04 16:17:46 -0300

    feat(sales-forecast): add useForecast hook composing BI providers (PRD-056)

---
+++ src/features/sales-forecast/index.ts
@@ -0,0 +1,8 @@
+export { computeForecast } from "./engine/computeForecast";
+export { buildForecastInput, type IBuildForecastInputArgs } from "./engine/buildForecastInput";
+export { DEFAULT_FORECAST_CONFIG } from "./engine/defaults";
+export {
+  useForecast,
+  type IUseForecastFilters,
+  type IUseForecastResult,
+} from "./hooks/useForecast";
