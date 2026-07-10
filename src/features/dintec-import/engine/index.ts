fa19bbc4d305269e7ab8ae6abf500af4cbaa865d
edmilson-prog <edmilson@ailainteligente.com>
2026-07-10 15:37:02 -0300

    feat: add DINTEC import engine barrel

---
+++ src/features/dintec-import/engine/index.ts
@@ -0,0 +1,5 @@
+export { normalizePhoneKey } from "./phoneKey";
+export { resolveCustomerType, type DintecCustomerType } from "./customerType";
+export { fillIfEmpty } from "./fillIfEmpty";
+export { normalizeVehicleBrandModel, type VehicleBrandModel } from "./vehicleNormalize";
+export { pickBestCodcliByLtv, type AmbiguousCandidate } from "./ambiguousTiebreak";
